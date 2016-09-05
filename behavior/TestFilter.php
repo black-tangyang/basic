2d37d78fe2cde21993236c3beb27085d3e6a4e4e
tangyang <tangyang@sochepiao.com>
2016-09-05 14:38:16 +0800

    行为

---
+++ behavior/TestFilter.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zx
+ * Date: 2016/9/5
+ * Time: 9:58
+ */
+
+namespace app\behavior;
+
+
+use Yii;
+use yii\base\Action;
+use yii\base\ActionFilter;
+
+class TestFilter  extends ActionFilter
+{
+    //在action之前运行，可用来过滤输入
+    public function beforeAction($action) {
+        echo '在调用action前显示<br/>';
+        return TRUE;//如果返回值为false,则action不会运行
+    }
+    //在action之后运行，可用来过滤输出
+    public function afterAction($action, $result) {
+        return $result.'在调用action后显示<br/>';//可以对action输出的$result进行过滤，retun的内容会直接显示
+    }
+
+}
---
+++ controllers/TestController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ty
+ * Date: 2016/9/5
+ * Time: 14:28
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class TestController extends Controller
+{
+    public function behaviors() {
+        return [
+            'test' => [
+                'class' => 'app\behavior\TestFilter'//调用过滤器
+            ]
+        ];
+    }
+    public function actionFilter() {
+        return '当前action显示<br/>';//返回的内容会递交给过滤器，由afterAction进行处理
+    }
+}
