a0f29174c1561ec67ed935fc913cb0c362b58aa8
tangyang <tangyang@sochepiao.com>
2016-09-18 11:22:18 +0800

    测试phpexcel

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -66,7 +66,7 @@ class SiteController extends Controller
 
 
     public function actionTest(){
-        $PHPexcel = new phpexcel();
+        $PHPexcel = new \PHPExcel();
         echo 'test';
     }
 
