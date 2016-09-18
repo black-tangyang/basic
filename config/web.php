96fcb89b2df23745c553bd1472f7ac285b350506
tangyang <tangyang@sochepiao.com>
2016-09-18 11:16:52 +0800

    测试phpexcel

--- config/web.php
+++ config/web.php
@@ -16,10 +16,10 @@ $config = [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'tangyang',
         ],
-        'urlManager' => [
+        /*'urlManager' => [
             'enablePrettyUrl' => true,
             //'showScriptName' => false,
-        ],
+        ],*/
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
