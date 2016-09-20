08ab147ef3e6664a15f7bd17843b69e0c387dc28
tangyang <tangyang@sochepiao.com>
2016-09-20 15:13:47 +0800

    测试phpexcel

--- config/db.php
+++ config/db.php
@@ -1,14 +1,9 @@
 <?php
 
 return [
-    /*'class' => 'yii\db\Connection',
+    'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=121.43.56.53;dbname=yii2basic',
     'username' => 'root',
     'password' => '123456',
-    'charset' => 'utf8',*/
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=120.132.70.111;dbname=leyou_flight',
-    'username' => 'hcp_dev',
-    'password' => 'a3befb2f897a3fd2f73fae02b102d0c5',
     'charset' => 'utf8',
 ];
