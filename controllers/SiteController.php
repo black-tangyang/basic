6cb76764056fc119b75d8b8729653662b8f068cd
tangyang <tangyang@sochepiao.com>
2016-10-10 16:41:25 +0800

    w

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -34,11 +34,24 @@ class SiteController extends Controller
     public function actionIndex()
     {
         define("TOKEN", "tangyangyangtest1125");
-        /*$echoStr = $_GET["echostr"];
-        if($this->checkSignature()){
-            echo $echoStr;
-            exit;
-        }*/
+
+        if(isset($_GET['echostr']))
+        {
+            $echoStr = $_GET["echostr"];
+
+            //valid signature , option
+            if($this->checkSignatureAction())
+            {
+                echo $echoStr;
+                exit;
+            }
+        }
+        else
+        {
+            $this->responseMsgAction();
+        }
+        return FALSE;
+
 
         $value = json_encode($_POST);
 
@@ -46,6 +59,45 @@ class SiteController extends Controller
         $str = $value;
         file_put_contents($path,$str,FILE_APPEND);
 
+    }
+
+    public function responseMsg()
+    {
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+
+        //extract post data
+        if (!empty($postStr)){
+            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+               the best way is to check the validity of xml by yourself */
+            libxml_disable_entity_loader(true);
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+							</xml>";
+            if(!empty( $keyword ))
+            {
+                $msgType = "text";
+                $contentStr = "Welcome to wechat world!";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }else{
+                echo "Input something...";
+            }
+
+        }else {
+            echo "";
+            exit;
+        }
     }
 
     private function checkSignature()
