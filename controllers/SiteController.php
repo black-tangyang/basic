361708e7c430e5df503e6555daadbd835c4db0bf
tangyang <tangyang@sochepiao.com>
2016-10-12 14:15:21 +0800

    test

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -34,7 +34,7 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
-        define("TOKEN", "tang");
+        define("TOKEN", "tangyangyangtest112521312");
 
         if(isset($_GET['echostr']))
         {
