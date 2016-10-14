b1acb080f71cb94b24164a48224710eb76d9aaed
tangyang <tangyang@sochepiao.com>
2016-10-14 10:16:00 +0800

    dfd

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -34,6 +34,12 @@ class SiteController extends Controller
 
     public function actionIndex()
     {
+
+        echo time();
+        echo "<pre>";
+        echo date('Y-m-d H:i:s');
+        exit;
+
         define("TOKEN", "tang");
 
         if(isset($_GET['echostr']))
